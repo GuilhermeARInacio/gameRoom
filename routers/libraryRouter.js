c1f0c8d84c38f40a3c58132f32741f7859ff8f9f
Guilherme Alves <guilherme.a.inacio@outlook.com>
2021-06-18 16:43:08 -0300

    alteracao pra verificar erro de not found da rota

--- routers/libraryRouter.js
+++ routers/libraryRouter.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
+    console.log('entrei')
     res.status(200)
     res.send(JSON.stringify('OK'))
+    console.log('entrei')
 })
 
 module.exports = router
